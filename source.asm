ac052f60711df9dc7597cfd618812aa758f25303
Paddy Withams <p.withams@gmail.com>
2016-06-10 15:32:52 -0600

    Finish problem 4 and start 5

--- source.asm
+++ source.asm
@@ -1,6 +1,6 @@
 ;---------------------------;
 ; Assignment 3				;
-; Problem 3					;
+; Problem 5					;
 ; Author: Patrick Withams	;
 ; Date: 9/6/2015			;
 ;---------------------------;
@@ -8,85 +8,168 @@
 include irvine32.inc
 
 .data
-	;-----------------------------------------------------
-	; the constant COUNT defines how many iterations of
-	; the sequence are needed
-	; fibArray stores the sequence, the length must
-	; match COUNT
-	; startVal is used for the 0, 1 positions of the
-	; sequence, before the loop takes place 
-	; titleMsg and fibMsg are used to print to screen
+	;-----------------------------------------------
+	; initialBal stores the users pre deposits/
+	; withdrawal balance
+	; deposits stores a list of user deposits
+	; withdrawals stores a list of user withdrawals
+	; finalBal stores the users updated balance
 	;
-	COUNT = 7				; max size is 24 due to 16bit WORD capacity
-	fibArray	WORD		COUNT DUP(?)
-	startVal	WORD		1	
-	titleMsg	BYTE		"Welcome to the Fibonacci Sequence generator!", 0
-	fibMsg		BYTE		"Fibonacci Sequence:", 0
+    msgTitle            BYTE        "Welcome to Balance Checker!", 0
+	origBalTitle		BYTE		"Your original balance: ", 0
+	balPrompt			BYTE		"Enter original balance (max 65535): ", 0
+	depPrompt			BYTE		"Enter deposits (max 24, enter 0 to finish): ", 0
+	withPrompt			BYTE		"Enter withdrawals (max 24, enter 0 to finish): ", 0
+	depTitle			BYTE		"Deposits: ", 0
+	withTitle			BYTE		"Withdrawals: ", 0
+    balanceTitle        BYTE        "Your new balance is: ", 0
+    initialBal          WORD        0
+    deposits            WORD        25 DUP(0)
+    withdrawals         WORD        25 DUP(0)
+    finalBal            WORD        0
 
 .code
 main proc
 	;------------------------
 	; print welcome message
 	;
-	mov EDX, OFFSET titleMsg									; move titleMsg address to EDX
-	call WriteString											; print "Welcome to the..." message to screen
+    mov EDX, OFFSET msgTitle
+    call WriteString
+    call Crlf
+	;-----------------------
+	; get original balance
+	;
+	mov EDX, OFFSET balPrompt
+	call WriteString
+	call ReadInt
+	mov initialBal, AX
+	;-------------------------
+	; print original balance
+	;
+	mov EDX, OFFSET origBalTitle
+	call WriteString
+	movzx EAX, initialBal
+	call WriteDec
 	call Crlf
-	;----------------------------------
-	; print start of sequence message
-	mov EDX, OFFSET fibMsg										; move fibMsg address to EDX
-	call WriteString											; print "Fibonacci Sequence:" message to screen
+	;---------------
+	; get deposits
+	;
+	mov EDX, OFFSET depPrompt
+	call WriteString
 	call Crlf
-	;------------------------------------
-	; move fibArray address to ESI, and
-	; push the first of two values of the
-	; sequence to the stack, and update
-	; the fibArray variable
+	mov EBX, LENGTHOF deposits
+	sub EBX, 1
+	mov ESI, OFFSET deposits
+	mov ECX, EBX
+EnterDeposits:
+	call ReadInt
+	cmp EAX, 0
+	je DepositTitle
+	mov [ESI], AX
+	add ESI, TYPE deposits
+	loop EnterDeposits
+	;-----------------
+    ; print deposits
 	;
-	mov ESI, OFFSET fibArray									; move fibArray address to ESI
-	mov EAX, 0													; set EAX to 0 to clear garbage value
-	mov AX, startVal											; move startVal (1) into 16bit register
-	mov [ESI], EAX												; set first element of fibArray to 1
-	add ESI, TYPE fibArray										; move address to next array element
-	mov [ESI], AX												; set second element of fibArray to 1
-	mov ECX, 0													; set ECX to 0 to clear garbage value
-	mov CX, COUNT												; set ECX (loop counter) to COUNT value
-	sub ECX, 2													; subtract 2 from counter value to account for first two elements
-	add ESI, TYPE fibArray										; move address to next array element
-	push EAX													; push the two starting values to the stack
-	push EAX
-	;-------------------------------
-	; start loop to calculate main
-	; sequence and update fibArray
+DepositTitle:	
+	mov EDX, OFFSET depTitle 
+	call WriteString
+	mov ECX, LENGTHOF deposits
+	mov ESI, OFFSET deposits
+	mov EAX, 0
+PrintDepositsLoop:
+	mov AX, [ESI]	
+	call WriteDec
+	cmp ECX, 1
+	je GetWithdrawals	
+	cmp AX, 0
+	je GetWithdrawals
+	mov AL, ','
+	call WriteChar
+	mov AL, ' '
+	call WriteChar
+	add ESI, TYPE WORD
+	dec ECX
+	jmp PrintDepositsLoop	
+	;------------------
+	; get withdrawals
 	;
-FibLoop:
-	pop EAX														; pop n-1 value into EAX
-	pop EBX														; pop n-2 value into EBX
-	push EAX													; push n-1 value back onto stack, to become n-2 for next loop
-	add EAX, EBX												; sum n-1 and n-2 to get n in EAX
-	mov [ESI], AX												; move n into fibArray current element position
-	add ESI, TYPE fibArray										; increment fibArray address
-	push EAX													; push n value onto stack, to become n-1 for next loop
-	loop FibLoop
-	;-----------------------------------
-	; move fibArray address to ESI and
-	; set ECX to array length
+GetWithdrawals:
+	call Crlf
+	mov EDX, OFFSET withPrompt
+	call WriteString
+	call Crlf
+	mov EBX, LENGTHOF withdrawals
+	sub EBX, 1
+	mov ESI, OFFSET withdrawals
+	mov ECX, EBX
+EnterWithdrawals:
+	call ReadInt
+	cmp EAX, 0
+	je WithdrawalsTitle
+	mov [ESI], AX
+	add ESI, TYPE withdrawals
+	loop EnterWithdrawals
+	;----------------------
+	; print withdrawals
 	;
-	mov ESI, OFFSET fibArray
-	mov ECX, LENGTHOF fibArray
-	;-----------------------------------
-	; loop over fibArray and print each
-	; array element
+WithdrawalsTitle:
+	mov EDX, OFFSET withTitle
+	call WriteString
+	mov ECX, LENGTHOF withdrawals
+	mov ESI, OFFSET withdrawals
+PrintWithdrawalsLoop:
+	mov AX, [ESI]
+	call WriteDec
+	cmp ECX, 1
+	je AddDeposits
+	cmp EAX, 0
+	je AddDeposits
+	mov AL, ','
+	call WriteChar
+	mov AL, ' '
+	call WriteChar
+	add ESI, TYPE WORD
+	dec ECX
+	jmp PrintWithdrawalsLoop
+	;--------------------------
+	; all deposits to initial
+	; balance value
 	;
-PrintLoop:
-	mov AX, [ESI]												; move current fibArray element value into AX
-	call WriteDec												; write value to screen
-	call Crlf													; print new line
-	add ESI, TYPE fibArray										; increment fibArray address	
-	loop PrintLoop
+AddDeposits:
+	movzx EAX, initialBal
+	mov ECX, LENGTHOF deposits
+	mov ESI, OFFSET deposits
+AddDepositsLoop:
+	add AX, [ESI]
+	add ESI, TYPE WORD
+	loop AddDepositsLoop
+	;--------------------------
+	; subtract withdrawals from
+	; updated balance value
+	;
+AddWithdrawals:
+	mov ECX, LENGTHOF withdrawals
+	mov ESI, OFFSET withdrawals
+AddWithdrawalsLoop:
+	sub AX, [ESI]
+	add ESI, TYPE WORD
+	loop AddWithdrawalsLoop
+	;-----------------------
+	; update final balance
+	; variable and print
+	; details to screen
+	;
+    mov finalBal, AX
+	call Crlf
+    mov EDX, OFFSET balanceTitle
+    call WriteString
+    call WriteDec
+    call Crlf
 	;---------------------------------------------
     ; wait for user input before exiting program
     ;
     call WaitMsg
-	exit
+    exit
 main endp
 end main
