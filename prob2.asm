93ca437be8fed31ac55c1b168b1d0ba661504afb
Paddy Withams <p.withams@gmail.com>
2016-06-09 19:29:53 -0600

    Add problem 3 source file

--- prob2.asm
+++ prob2.asm
@@ -1,75 +1,75 @@
-; Working source file
+;---------------------------;
+; Assignment 3				;
+; Problem 2					;
+; Author: Patrick Withams	;
+; Date: 9/6/2015			;
+;---------------------------;
 
 include irvine32.inc
 
 .data
-   myArray		DWORD		0Fh, 1, 2, 10h, 5, 14h	
-   revArray		DWORD		LENGTHOF myArray DUP(?)
-   origMsg		BYTE		"Original Array: ", 0
-   revMsg		BYTE		"Reversed Array: ", 0
-   copyMsg		BYTE		"The original array, now copied from reversed array: ", 0
+	;-----------------------------------------------------
+	; myArray is the variable that will be reversed
+	; origMsg and revMsg are strings that are displayed
+	; before and after the array is reversed
+	;
+	myArray		DWORD		0Fh, 1, 2, 10h, 5, 14h	
+	origMsg		BYTE		"Original Array: ", 0
+	revMsg		BYTE		"Original Array, now reversed: ", 0
 
 .code
 main proc
-	; print original array
-	mov EDX, OFFSET origMsg
-	call WriteString
+    ;-----------------------
+    ; print original array
+	;
+	mov EDX, OFFSET origMsg						; move "Original Array: " message address to EDX 
+	call WriteString							; print to screen
 	call Crlf
-	mov ECX, LENGTHOF myArray
-	mov ESI, OFFSET myArray
-	PrintOrigArray:
-		mov EAX, [ESI]
-		add ESI, TYPE myArray
-		call WriteDec
-		call Crlf
-		loop PrintOrigArray
-	; reverse original array into revArray variable	
-	mov ESI, OFFSET revArray
-	mov ECX, LENGTHOF myArray
-	mov EBX, ESI
-	sub EBX, TYPE myArray
-	ReverseArray:
-		mov EAX, [EBX]
-		mov [ESI], EAX
-		add ESI, TYPE myArray
-		sub EBX, TYPE myArray
-		loop ReverseArray
-	; print revArray to show reversed result
-	mov EDX, OFFSET revMsg
-	call WriteString
-	call Crlf		
-	mov ECX, LENGTHOF revArray
-	mov ESI, OFFSET revArray
-	PrintRevArray:
-		mov EAX, [ESI]
-		add ESI, TYPE revArray
-		call WriteDec
-		call Crlf
-		loop PrintRevArray
-	; if required, copy reversed array into original array memory so that myArray is now reversed
-	mov ECX, LENGTHOF revArray
-	mov ESI, OFFSET revArray
-	mov EBX, OFFSET myArray
-	UpdateOriginal:
-		mov EAX, [ESI]
-		mov [EBX], EAX
-		add ESI, TYPE revArray
-		add EBX, TYPE revArray
-		loop UpdateOriginal
-	; print myArray to show that original variable is now reversed	
-	mov EDX, OFFSET copyMsg
-	call WriteString
+	mov ECX, LENGTHOF myArray					; set ECX register (decremented each loop) to length of array
+	mov ESI, OFFSET myArray						; set ESI to the first memory address of array
+PrintOrigArray:
+    mov EAX, [ESI]								; move element value into EAX
+    add ESI, TYPE myArray						; move ESI address to next element by adding array type (1 or 2 etc)
+    call WriteDec								; write EAX to screen
+    call Crlf
+    loop PrintOrigArray
+    ;-----------------------------------
+    ; push each element onto the stack 
+    ;
+	mov ESI, OFFSET myArray						; set ESI to the first memory address of array
+	mov ECX, LENGTHOF myArray					; set ECX (loop counter) to length of array
+PushArray:
+    push [ESI]									; for each element address, push the value onto the stack
+    add ESI, TYPE myArray
+    loop PushArray
+    ;---------------------------------
+    ; pop each element off the stack 
+    ;
+    mov ESI, OFFSET myArray						; set ESI to the first memory address of array
+	mov ECX, LENGTHOF myArray					; set ECX (loop counter) to length of array
+PopArray:
+    pop EAX										; for each loop, pop stack value into EAX
+    mov [ESI], EAX								; then move EAX value into each array element 
+    add ESI, TYPE myArray						; increment array element address
+    loop PopArray
+    ;---------------------------------------
+    ; print myArray to show it is reversed 
+    ;
+	mov EDX, OFFSET revMsg						; move "Original Array, now reversed: " message to EDX
+	call WriteString							; print message to screen
 	call Crlf	
-	mov ECX, LENGTHOF myArray
-	mov ESI, OFFSET myArray
-	PrintCopyArray:
-		mov EAX, [ESI]
-		add ESI, TYPE myArray
-		call WriteDec
-		call Crlf
-		loop PrintCopyArray
-	; wait for user input before exiting program
-	call WaitMsg
+	mov ECX, LENGTHOF myArray					; set ECX (loop counter) to length of array
+	mov ESI, OFFSET myArray						; set ESI to first memory address of array
+PrintCopyArray:
+    mov EAX, [ESI]								; for each loop, move element value into EAX
+    add ESI, TYPE myArray						; increment address value to next element
+    call WriteDec								; write EAX value to screen
+    call Crlf
+    loop PrintCopyArray
+    ;---------------------------------------------
+    ; wait for user input before exiting program
+    ;
+    call WaitMsg
 	exit
 main endp
 end main
